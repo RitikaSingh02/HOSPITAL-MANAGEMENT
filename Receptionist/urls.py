b9d54210c134c57f58077b48c2562fcb4bc83de2
RitikaSingh02 <ritika2002singh@gmail.com>
2020-06-17 19:48:27 +0530

    added Receptionist interface, applied joins

--- Receptionist/urls.py
+++ Receptionist/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     url(r'^$',views.receptionists_list,name="receptionists_list"),
     url(r'^Receptionist_login/$',views.receptionist_login,name="receptionist_login"),
     url(r'^pending_appointments/$',views.pending_appointments,name="pending_appointments"),
-
-    
+    url(r'^approve_appointments/$',views.approve_appointment,name="approve_appointments"),
+    url(r'^reject_appointments/$',views.reject_appointment,name="reject_appointments"),
 ]
--- Receptionist/views.py
+++ Receptionist/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from . models import receptionists
-from Patient.models import patients
+from Patient.models import patients,patient_appointment_details
 import json
 
 def receptionists_list(request):
@@ -23,11 +23,28 @@ def receptionist_login(request):
         else:
             return JsonResponse("failed",safe=False)
 
-#def receptionist_dashboard(request):
-
 
 def pending_appointments(request):
     if request.method=="GET":
-        patients=patients.objects.filter(APPOINTMENT_STATUS="PENDING").values()
-        return JsonResponse(list(patients),safe=False)
-# Create your views here.
+
+        patient=patients.objects.filter(APPOINTMENT_STATUS_RECEPTIONIST="PENDING",status='active').values('PATIENT_FIRST_NAME',
+        'PATIENT_LAST_NAME',"patient_appointment_details__appointment_date","patient_appointment_details__appointment_time",
+        "id","patient_appointment_details__speciality","patient_appointment_details__preferred_location",
+        "patient_appointment_details__problem").order_by("patient_appointment_details__id")
+        
+        
+        return JsonResponse(list(patient),safe=False)
+
+def approve_appointment(request):
+    if request.method=="POST":
+        data=json.loads(request.body)
+        patient=patients.objects.filter(id=data['id']).update(APPOINTMENT_STATUS_RECEPTIONIST="APPROVED",
+        APPOINTMENT_STATUS_DOCTOR="PENDING")
+        return JsonResponse("your request has been approved by the manager",safe=False)
+
+def reject_appointment(request):
+    if request.method=="POST":#u have to request id and the message for rejection 
+        data=json.loads(request.body)
+        patient=patients.objects.filter(id=data['id']).update(APPOINTMENT_STATUS_RECEPTIONIST="REJECTED")
+        return JsonResponse("your request has been approved by the manager because of the reason "+str(data['reason']),safe=False)
+
